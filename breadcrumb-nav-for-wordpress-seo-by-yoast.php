2347028ca5507a85546e169af458fc3831e6ddd5
Sii <info@onlineprofiler.de>
2013-10-01 10:27:36 +0200

    format change
    change of nomenklatur

---
+++ breadcrumb-nav-for-wordpress-seo-by-yoast.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	Plugin Name: Breadcrumb NAV for WordPress SEO by Yoast
+	Author: Online Profiler
+	Author URI: http://www.onlineprofiler.de
+	Description: Build for WordPress SEO by Yoast - this section will display a breadcrumb navigation for better usability and SEO benefits. It requires the installation of the plugin "WordPress SEO" by Yoast and creates within PageLines DMS a section, that can be placed via drag & drop wherever you need it.
+	Class Name: WordPressSEOBreadcrumb
+	Demo:
+	Version: 1.0
+	Filter: nav
+*/
--- sections/breadcrumb-nav-for-wordpress-seo-by-yoast/section.php
+++ sections/breadcrumb-nav-for-wordpress-seo-by-yoast/section.php
@@ -5,7 +5,7 @@
 	Author URI: http://www.onlineprofiler.de
 	Description: Build for WordPress SEO by Yoast - this section will display breadcrumb navigation for better usability and SEO benefit. It requires the installation of the plugin "WordPress SEO" by Yoast and creates within PageLines DMS a section, that can be placed via drag & drop wherever you need it.
 	Class Name: WordPressSEOBreadcrumb
-	Demo: http://www.onlineprofiler.de/portfolio/projekte/onlineprofiler/
+	Demo:
 	Version: 1.0
 	Filter: nav
 	v3: true
