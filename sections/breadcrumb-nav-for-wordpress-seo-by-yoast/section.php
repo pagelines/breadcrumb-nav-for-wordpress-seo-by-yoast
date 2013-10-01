43ced4f5d1f14e22550c48c9915fc8f09c7223df
Sii <info@onlineprofiler.de>
2013-10-01 18:37:52 +0200

    spelling check
    spelling check

--- sections/breadcrumb-nav-for-wordpress-seo-by-yoast/section.php
+++ sections/breadcrumb-nav-for-wordpress-seo-by-yoast/section.php
@@ -3,7 +3,7 @@
 	Section: Breadcrumb
 	Author: Online Profiler
 	Author URI: http://www.onlineprofiler.de
-	Description: Build for WordPress SEO by Yoast - this section will display breadcrumb navigation for better usability and SEO benefit. It requires the installation of the plugin "WordPress SEO" by Yoast and creates within PageLines DMS a section, that can be placed via drag & drop wherever you need it.
+	Description: Build for WordPress SEO by Yoast - this section will display a breadcrumb navigation for better usability and SEO benefits. It requires the installation of the plugin "WordPress SEO" by Yoast and creates within PageLines DMS a section, that can be placed via drag & drop wherever you need it.
 	Class Name: WordPressSEOBreadcrumb
 	Demo:
 	Version: 1.0
