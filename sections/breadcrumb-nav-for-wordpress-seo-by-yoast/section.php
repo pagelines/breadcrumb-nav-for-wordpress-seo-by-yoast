a5d410d1d90e9e24df1365a4e31ec565def8c4e5
Sii <info@onlineprofiler.de>
2014-03-21 12:15:28 +0100

    Update for DMS2
    change of icon font and description

--- sections/breadcrumb-nav-for-wordpress-seo-by-yoast/section.php
+++ sections/breadcrumb-nav-for-wordpress-seo-by-yoast/section.php
@@ -3,10 +3,10 @@
 	Section: Breadcrumb
 	Author: Online Profiler
 	Author URI: http://www.onlineprofiler.de
-	Description: Built for WordPress SEO by Yoast - this section will display breadcrumb navigation for better usability and SEO benefit. It requires the installation of the plugin "WordPress SEO" by Yoast and creates a PageLines DMS section that can be placed via drag & drop.
+	Description: <strong>Use it with DMS2</strong><br />Built for WordPress SEO by Yoast - this section will display breadcrumb navigation for better usability and SEO benefit. It requires the installation of the plugin "WordPress SEO" by Yoast and creates a PageLines DMS section that can be placed via drag & drop.<br />Find all Versions of Breadcrumb on <a href="http://www.onlineprofiler.de/technik/neues-dms-section-breadcrumb-nav/" target="_blank">www.onlineprofiler.de</a>
 	Class Name: WordPressSEOBreadcrumb
 	Demo:
-	Version: 1.1
+	Version: 1.2
 	Filter: nav
 	Loading: active
 	Cloning: true
@@ -15,7 +15,7 @@
 
 class WordPressSEOBreadcrumb extends PageLinesSection {
 
-	const version = '1.1'; 
+	const version = '1.2'; 
 
 
 // RUNS IN <HEAD> 
@@ -38,7 +38,7 @@ class WordPressSEOBreadcrumb extends PageLinesSection {
 
 		ob_start();
 
-		?><div style="font-size:12px;"><?php _e('Thank you for using Breadcrumb NAV for WordPress SEO by Yoast!<br />Just follow the instructions to set it up.<br />Have fun!<br /><br />This section is free but of course it took me some time to create it. If you feel like saying thank you, I would be more then happy if you could donate to <strong>"Berliner Stadtkatzen"</strong>. This project tries to help stray cats throughout Berlin to get medical treatments.<br />You might find the donation button on my site<br /><strong><a href="http://www.onlineprofiler.de/sozial/" target="_blank" title="Online Profiler | Social Projects">www.onlineprofiler.de</a></strong>.<br />Thank you!','breadcrumb-nav-for-wordpress-seo-by-yoast');?></div><?php
+		?><div style="font-size:12px;"><?php _e('<strong>Version: 1.2 Please use it only with DMS 2!</strong><br />Thank you for using Breadcrumb NAV for WordPress SEO by Yoast!<br />Just follow the instructions to set it up.<br />Have fun! <br /><br /> If you might need older Breadcrumb Versions, please find them on my website <br /><a href="http://www.onlineprofiler.de/technik/neues-dms-section-breadcrumb-nav/" target="_blank" alt="Online Profiler | Breadcrumb">&rarr; All Versions of the Breadcumb section on www.onlineprofiler.de</a><br /><br />Thank you! ','breadcrumb-nav-for-wordpress-seo-by-yoast');?></div><?php
 
 		return ob_get_clean();
 	}
@@ -70,7 +70,7 @@ class WordPressSEOBreadcrumb extends PageLinesSection {
 			'opts'	=> array(
 				array(
 					'key'		=> 'set_wpseo',
-					'label'		=> '<i class="icon-download "></i> Download WordPress SEO by Yoast ',
+					'label'		=> '<i class="fa fa-download"></i> Download WordPress SEO by Yoast ',
 					'type' 		=> 'link',
 					'classes'	=> 'btn-primary btn-block',
 					'url'		=> 'http://wordpress.org/plugins/wordpress-seo/' ,
@@ -80,7 +80,7 @@ class WordPressSEOBreadcrumb extends PageLinesSection {
 				),
 				array(
 					'key'		=> 'set_breadcrumb',
-					'label'		=> '<i class="icon-dashboard "></i> Setup WordPress SEO Breadcrumb ',
+					'label'		=> '<i class="fa fa-dashboard"></i> Setup WordPress SEO Breadcrumb ',
 					'type' 		=> 'link',
 					'classes'	=> 'btn-primary btn-block',
 					'url'		=> admin_url( 'admin.php?page=wpseo_internal-links' ),
